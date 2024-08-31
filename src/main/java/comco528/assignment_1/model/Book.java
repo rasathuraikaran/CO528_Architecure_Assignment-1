5cf4bfdadc2d37faa7e30a6a7d51a36ca34b7a9b
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2024-08-31 08:46:33 +0530

    Book model updated

---
+++ src/main/java/comco528/assignment_1/model/Book.java
@@ -0,0 +1,24 @@
+package comco528.assignment_1.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+
+    // Getters and Setters implemented using Lombok anotation @Getter and @Setter
+}
